8ef81325e9d4261780d9645ea12d8b4a29224f66
huanglei <huanglei@quanjingshuju.com>
2021-04-19 09:03:18 +0800

    WIP: 初始提交

---
+++ phinx.php
@@ -0,0 +1,24 @@
+<?php
+//读取当前系统设置
+$env = parse_ini_file('.env', true);
+
+return [
+    "paths" => [
+        "migrations" => "database/migrations",
+        "seeds" => "database/seeds"
+    ],
+    "environments" => [
+        "default_migration_table" => "phinxlog",
+        "default_database" => "dev",
+        "default_environment" => "dev",
+        "dev" => [
+            "adapter" => $env["DB_CONNECTION"],
+            "host" => $env["DB_HOST"],
+            "name" => $env["DB_DATABASE"],
+            "user" => $env["DB_USERNAME"],
+            "pass" => $env["DB_PASSWORD"],
+            "port" => $env["DB_PORT"],
+            "charset" => "utf8"
+        ]
+    ]
+];
